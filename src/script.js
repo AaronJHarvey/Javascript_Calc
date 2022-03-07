466fecf80f0ce015365a9db0b8348f9fa7052c2b
Aaron Harvey <Harvey.AaronJ@gmail.com>
2022-03-07 17:15:34 -0500

    lays out functions that will be built

---
+++ src/script.js
@@ -0,0 +1,30 @@
+class Calculator {
+  constructor(previousOperAndTextElement, currentOperAndTextElement) {
+    this.previousOperAnd = previousOperAndTextElement;
+    this.currentOperAnd = currentOperAndTextElement;
+  }
+
+  clear() {}
+
+  delete() {}
+
+  appendNumber(number) {}
+
+  chooseOperation(operation) {}
+
+  compute() {}
+
+  updatedDisplay() {}
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperAndTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const CurrentOperAndTextElement = document.querySelector(
+  "[data-current-operand]"
+);
