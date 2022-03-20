7c8866fb5877931a3cfec6ab22de829493d92cf1
AaronJHarvey <Harvey.AaronJ@gmail.com>
2022-03-20 19:02:20 -0400

    adds event listener to delete button to get it working

--- src/script.js
+++ src/script.js
@@ -102,3 +102,8 @@ allClearButton.addEventListener( 'click', button => {
   calculator.clear()
   calculator.updateDisplay()
 })
+
+deleteButton.addEventListener('click', button => {
+  calculator.delete()
+  calculator.updateDisplay()
+})
